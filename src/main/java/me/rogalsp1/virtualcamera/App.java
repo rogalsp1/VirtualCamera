f4e61cd7c5126827c4e9538df0003f00ec30ab6e
rogalsp1 <rogalsp1@ee.pw.edu.pl>
2016-10-15 11:40:13 +0200

    Initial commit

---
+++ src/main/java/me/rogalsp1/virtualcamera/App.java
@@ -0,0 +1,13 @@
+package me.rogalsp1.virtualcamera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
